8333f82cdb5462c6365cf37fbc22bac26c809e7d
Kelsey McDonough <kelseyjo@san.rr.com>
2021-02-16 14:40:50 -0800

    initial commit
    background image, player movement, simple level.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Character_Controller_2D controller;
+    float horizontal_move = 0;
+    bool jump = false;
+    public float run_speed = 40;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get input from player
+        horizontal_move = CrossPlatformInputManager.GetAxis("Horizontal") * run_speed;
+        if(CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        // if(Input.GetButtonDown)
+    }
+
+    void FixedUpdate()
+    {
+        //move player (apply input to player)
+        controller.Move((horizontal_move * Time.fixedDeltaTime), false, jump); //character controller script method
+        jump = false;
+
+    }
+}
